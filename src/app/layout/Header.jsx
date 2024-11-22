ffcc18e9e2252c8ca42bcb4c1cfee7a40092cbc0
MENG Vuthvitou <mengvuthvitou11@gmail.com>
2024-11-22 15:38:04 +0700

    Add Layout Header

--- src/app/layout/Header.jsx
+++ src/app/layout/Header.jsx
@@ -1,5 +1,33 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="container-fluid position-fixed w-100 header d-flex justify-content-end align-items-center p-1 pe-5">
+      <div className="btn-group">
+        <button
+          className="dropdown-toggle border-0 text-light fw-bolder"
+          type="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <span className="text-uppercase">Admin</span>
+        </button>
+
+        <ul className="dropdown-menu border-0 py-0 rounded-2 shadow overflow-hidden">
+          <li>
+            <Link to="/" className="dropdown-item dropdown-custome">
+              Profile
+            </Link>
+          </li>
+          <li>
+            <span className="dropdown-item border-top dropdown-logout">
+              Logout
+            </span>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
