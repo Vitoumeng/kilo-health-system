03ae2311990d5eb915487da42e0cd420e8cf051f
MENG Vuthvitou <mengvuthvitou11@gmail.com>
2024-11-25 15:46:47 +0700

    Add Sweetalert on Login, Logout and Add Some Style on Header

--- src/app/module/auth/core/action.js
+++ src/app/module/auth/core/action.js
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 import { setAuth, setLogin } from "./reducer";
 import { reqLogin } from "./request";
+import Swal from "sweetalert2";
 
 const useLogin = () => {
   const dispatch = useDispatch();
@@ -20,17 +21,36 @@ const useLogin = () => {
       dispatch(setAuth(response));
       localStorage.setItem("user", JSON.stringify(response.user));
       localStorage.setItem("accessToken", response.token);
-      navigate("/");
+      Swal.fire({
+        icon: "success",
+        title: "Login Successful",
+        text: "Welcome back!",
+        confirmButtonText: "OK",
+      }).then(() => {
+        navigate("/");
+      });
     } catch (err) {
       console.log("Error");
     }
   };
 
   const onLogout = () => {
-    dispatch(setAuth(undefined));
-    localStorage.removeItem("user");
-    localStorage.removeItem("accessToken");
-    navigate("/login");
+    Swal.fire({
+      title: "Are you sure?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "lightcoral",
+      confirmButtonText: "OK",
+      cancelButtonColor: "lightgrey",
+      cancelButtonText: "Cancel",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        dispatch(setAuth(undefined));
+        localStorage.removeItem("user");
+        localStorage.removeItem("accessToken");
+        navigate("/login");
+      }
+    });
   };
 
   return { ...auth, handleChangeLogin, onLogin, onLogout };
