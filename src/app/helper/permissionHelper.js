1114ee3732312bdef71766f2a2e65f534d713b65
MENG Vuthvitou <liltou081@gmail.com>
2024-12-31 10:10:21 +0700

    Add Function Check iif User has Permission or not

---
+++ src/app/helper/permissionHelper.js
@@ -0,0 +1,31 @@
+import _ from "lodash";
+
+const getPermissions = () => {
+  const permissions = JSON.parse(localStorage.getItem("permissions"));
+
+  if (!permissions) return [];
+
+  return permissions;
+};
+
+const hasPermission = (pers = "") => {
+  const permissions = getPermissions();
+
+  if (_.isEmpty(permissions)) {
+    return false;
+  }
+
+  return permissions.find((per) => per.name === pers)?.status ?? false;
+};
+
+const HasPermissionAction = ({ children, permission }) => {
+  if (hasPermission(permission)) {
+    return children;
+  } else if (permission === "View-All") {
+    return children;
+  }
+
+  return null;
+};
+
+export default HasPermissionAction;
