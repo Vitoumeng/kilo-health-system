3bf72c8e46e2ce98e772a6f50899a17e547cdc9e
MENG Vuthvitou <liltou081@gmail.com>
2025-01-07 14:13:37 +0700

    Add File Image on File Table

--- src/app/module/file-upload/component/Table.jsx
+++ src/app/module/file-upload/component/Table.jsx
@@ -12,13 +12,6 @@ const Table = ({ data, handleDelete, navigate }) => {
               className="text-start fw-semibold text-light"
               style={{ fontSize: "14px" }}
             >
-              File Name
-            </th>
-            <th
-              scope="col"
-              className="text-start fw-semibold text-light"
-              style={{ fontSize: "14px" }}
-            >
               File Type
             </th>
             <th
@@ -27,6 +20,13 @@ const Table = ({ data, handleDelete, navigate }) => {
               style={{ fontSize: "14px" }}
             >
               File Size
+            </th>
+            <th
+              scope="col"
+              className="text-start fw-semibold text-light"
+              style={{ fontSize: "14px" }}
+            >
+              File Image
             </th>
             <th
               scope="col"
@@ -44,7 +44,7 @@ const Table = ({ data, handleDelete, navigate }) => {
                 className="text-start fw-medium text-light"
                 style={{ fontSize: "14px" }}
               >
-                {item?.fileName}
+                {item?.fileType}
               </td>
               <td
                 className="text-start fw-medium text-light"
@@ -52,11 +52,19 @@ const Table = ({ data, handleDelete, navigate }) => {
               >
                 {item?.fileSize}
               </td>
-              <td
-                className="text-start fw-medium text-light"
-                style={{ fontSize: "14px" }}
-              >
-                {item?.fileType}
+              <td className="text-start fw-medium text-secondary">
+                <img
+                  src={item?.fileUrl}
+                  alt=""
+                  style={{
+                    width: "35px",
+                    height: "35px",
+                    background: "#fff",
+                    borderRadius: "12px",
+                    overflow: "hidden",
+                  }}
+                  className=" object-fit-cover rounded-2 overflow-hidden"
+                />
               </td>
               <td className="d-flex justify-content-end gap-2">
                 <HasPermissionAction permission="Delete-File">
